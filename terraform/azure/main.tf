df688bf671a46769b91610e7c39729c13d20c7c6
PRADEEP R <pradeep2005ravi@gmail.com>
2026-03-10 21:25:02 +0530

    Update main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -7,9 +7,7 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-}
+provider "azurerm" { features {} }
 
 resource "azurerm_resource_group" "rg" {
   name     = "multi-cloud-rg"
@@ -34,52 +32,15 @@ resource "azurerm_network_security_group" "nsg" {
   name                = "multi-cloud-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule { name="SSH"        priority=100 direction="Inbound" access="Allow" protocol="Tcp" source_port_range="*" destination_port_range="22"   source_address_prefix="*" destination_address_prefix="*" }
+  security_rule { name="Flask"      priority=110 direction="Inbound" access="Allow" protocol="Tcp" source_port_range="*" destination_port_range="5000" source_address_prefix="*" destination_address_prefix="*" }
+  security_rule { name="Prometheus" priority=120 direction="Inbound" access="Allow" protocol="Tcp" source_port_range="*" destination_port_range="9090" source_address_prefix="*" destination_address_prefix="*" }
+  security_rule { name="Grafana"    priority=130 direction="Inbound" access="Allow" protocol="Tcp" source_port_range="*" destination_port_range="3000" source_address_prefix="*" destination_address_prefix="*" }
 }
 
-resource "azurerm_network_security_rule" "flask_port" {
-  name                        = "allow-flask"
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "5000"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.nsg.name
-}
-
-resource "azurerm_network_security_rule" "prometheus_port" {
-  name                        = "allow-prometheus"
-  priority                    = 110
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "9090"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.nsg.name
-}
-
-resource "azurerm_network_security_rule" "grafana_port" {
-  name                        = "allow-grafana"
-  priority                    = 120
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "3000"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.nsg.name
-}
-
-resource "azurerm_public_ip" "public_ip" {
-  name                = "multi-cloud-public-ip"
+resource "azurerm_public_ip" "pip" {
+  name                = "multi-cloud-pip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
@@ -90,42 +51,28 @@ resource "azurerm_network_interface" "nic" {
   name                = "multi-cloud-nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip.id
+    public_ip_address_id          = azurerm_public_ip.pip.id
   }
-
-  depends_on = [
-    azurerm_subnet.subnet,
-    azurerm_public_ip.public_ip
-  ]
 }
 
 resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
-
-  depends_on = [
-    azurerm_network_interface.nic,
-    azurerm_network_security_group.nsg
-  ]
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
   name                            = "multi-cloud-azure-vm"
   resource_group_name             = azurerm_resource_group.rg.name
   location                        = azurerm_resource_group.rg.location
-  size                            = "Standard_D2s_v3"
+  size                            = "Standard_B1s"
   admin_username                  = "azureuser"
   admin_password                  = "Password1234!"
   disable_password_authentication = false
-
-  network_interface_ids = [
-    azurerm_network_interface.nic.id
-  ]
+  network_interface_ids           = [azurerm_network_interface.nic.id]
 
   os_disk {
     caching              = "ReadWrite"
@@ -139,30 +86,39 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 
-  custom_data = base64encode(<<-EOF
+  custom_data = base64encode(<<EOF
 #!/bin/bash
-apt update -y
-apt install docker.io git -y
+apt-get update -y
+apt-get install -y docker.io git
 systemctl start docker
 systemctl enable docker
+sleep 15
+cd /home/azureuser
 git clone https://github.com/pradeep435/multi-cloud-devops.git
 cd multi-cloud-devops
-docker build -t multi-cloud-app .
-docker run -d -p 5000:5000 --restart=always multi-cloud-app
-docker run -d -p 9090:9090 --restart=always prom/prometheus
-docker run -d -p 3000:3000 --restart=always grafana/grafana
+cat > /home/azureuser/prometheus.yml <<PROM
+global:
+  scrape_interval: 15s
+scrape_configs:
+  - job_name: 'streamcloud'
+    static_configs:
+      - targets: ['172.17.0.1:5000']
+    metrics_path: '/metrics'
+  - job_name: 'prometheus'
+    static_configs:
+      - targets: ['localhost:9090']
+PROM
+docker build -t streamcloud-app .
+docker run -d --name flask-app --restart=always -p 5000:5000 -e CLOUD_PROVIDER=AZURE streamcloud-app
+docker run -d --name prometheus --restart=always -p 9090:9090 -v /home/azureuser/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
+docker run -d --name grafana --restart=always -p 3000:3000 -e GF_SECURITY_ADMIN_PASSWORD=admin grafana/grafana
 EOF
   )
 
-  depends_on = [
-    azurerm_network_interface_security_group_association.nsg_assoc
-  ]
+  tags = { Name = "StreamCloud-Standby", Project = "MultiCloudDevOps" }
 }
 
-output "azure_vm_public_ip" {
-  value = azurerm_public_ip.public_ip.ip_address
-}
-
-output "app_url" {
-  value = "http://${azurerm_public_ip.public_ip.ip_address}:5000"
-}
+output "azure_vm_public_ip" { value = azurerm_public_ip.pip.ip_address }
+output "app_url"            { value = "http://${azurerm_public_ip.pip.ip_address}:5000" }
+output "prometheus_url"     { value = "http://${azurerm_public_ip.pip.ip_address}:9090" }
+output "grafana_url"        { value = "http://${azurerm_public_ip.pip.ip_address}:3000" }
